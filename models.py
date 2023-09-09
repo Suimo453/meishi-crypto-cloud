64d6c5c788fe89bba3366a830631976242f5ebb7
atsu0453 <yoneatsu1213@yahoo.co.jp>
2023-09-09 13:53:58 +0900

    no message

--- models.py
+++ models.py
@@ -1,14 +1,28 @@
-from testapp import db
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer, String, Float, BigInteger
+from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+from sqlalchemy import DateTime
+Base = declarative_base()
+engine = create_engine(f'sqlite:///db.sqlite3')
+SessionClass = sessionmaker(engine)
+session = SessionClass()
 
+class Meishi(Base):
+    __tablename__ = 'meishi'
+    company = Column(String(255))
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String(255))
+    lastname = Column(String(255))
+    mail = Column(String(255))
+    department = Column(String(255))
+    picture = Column(String(50*1000*1000))
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
+    updated_at = Column(DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
-class Employee(db.Model):
-    __tablename__ = 'employee'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    mail = db.Column(db.String(255))
-    is_remote = db.Column(db.Boolean)
-    department = db.Column(db.String(255))
-    year = db.Column(db.Integer, default=0)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+Base.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    pass
