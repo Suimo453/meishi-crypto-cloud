befe3e9e9031512dc58cd4ec02d888e9047ec3ad
atsu0453 <yoneatsu1213@yahoo.co.jp>
2023-09-09 16:10:34 +0900

    no message

--- models.py
+++ models.py
@@ -8,6 +8,23 @@ class Meishi():
         self.image = image
         self.name_key = name_key
         self.company_key = company_key
+    
+    def to_dict(self):
+        return self.__dict__
+
+    def from_dict(d):
+        self = Meishi("", "", "", "", "")
+        self.__dict__ = d
+        return self
+    
+    def __str__(self):
+        return f'''=== Meishi ===
+name = {self.name}
+company = {self.company}
+image = omit
+name_key = {self.name_key}
+company_key = {self.company_key}
+'''
 
 def select_all():
     with open(DB_FILE_NAME) as f:
@@ -19,8 +36,19 @@ def save(detabase):
 
 def add(meishi):
     detabase = select_all()
-    detabase.append(meishi.__dict__)
+    detabase.append(meishi.to_dict())
     save(detabase)
+
+def select_by_name_key(name_key):
+    detabase = select_all()
+    result = []
+    for line in detabase:
+        meishi = Meishi.from_dict(line)
+        if meishi.name_key == name_key:
+            result.append(meishi)
+
+    return result
+
 
 
 if __name__ == "__main__":
@@ -28,4 +56,8 @@ if __name__ == "__main__":
     meishi2 = Meishi(company="kaishaA",name="namaeA",image="aaaaaa",name_key="namaekagiA",company_key="kaishakagiA")
     add(meishi1)
     add(meishi2)
-    print(select_all())
+    ## print(select_all())
+
+    meishi = select_by_name_key(name_key = "namaekagi")
+    print(meishi[0])
+    
